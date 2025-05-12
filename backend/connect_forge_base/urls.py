fa99aad16b717650bfaee4d342d04b70dc2354f2
ShalinAli <shalinamirali@folio3.com>
2025-05-12 11:47:26 +0500

    backend project files restructured

--- backend/connect_forge_base/urls.py
+++ backend/connect_forge_base/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for agent_api project.
+URL configuration for connect_forge_base project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
